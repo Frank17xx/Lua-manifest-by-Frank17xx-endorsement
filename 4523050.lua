a84470c5d9ca53b4c576853ae4935bf2f4ebd642
Frank17xx <juanyanac52@gmail.com>
2026-08-12 18:00:51 -0500

    Add files via upload

---
+++ 4523050.lua
@@ -0,0 +1,22 @@
+-- 4523050's Lua and Manifest Created by Hubcap Manifest
+-- EA SPORTS FC™ 27 Closed Beta
+-- Created: August 12, 2026 at 06:35:51 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 5
+-- Total DLCs: 0
+-- Shared Depots: 3
+
+-- MAIN APPLICATION
+addappid(4523050) -- EA SPORTS FC™ 27 Closed Beta
+-- MAIN APP DEPOTS
+addappid(4523051, 1, "5785685b0232fea14079d3beac2b30f6cadadf37913a0201c3e9a46e41d4ebfd") -- Depot 4523051
+setManifestid(4523051, "5158753818507526757", 76717354106)
+addappid(4523052, 1, "f90d638639ebc0755c6620d522b8054ffdf2f2674beb7f6cd81101bde52ea7be") -- Depot 4523052
+setManifestid(4523052, "2465416239659095776", 194053)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "5753583882400741046", 25674515)
+addappid(3340991, 1, "023daedb070e5af8704dc88ee0af829f5c11923d2e6a42cca11ba5713b9f4491") -- Depot 3340991 (Shared from App 3340990)
+setManifestid(3340991, "2111133955805833191", 241196903)
+addappid(3893181, 1, "7f675c2fe8e758d16f3cf8d3b493956afaee78e96d78cdb668a5edb8ac1580f6") -- Depot 3893181 (Shared from App 3893180)
+setManifestid(3893181, "1558442456104985494", 333590753)
