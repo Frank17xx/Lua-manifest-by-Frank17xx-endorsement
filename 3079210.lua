b024e643ec6a200bfddc47a47f8e2b803fb5481a
Frank17xx <juanyanac52@gmail.com>
2026-08-13 17:47:12 -0500

    Add files via upload

---
+++ 3079210.lua
@@ -0,0 +1,17 @@
+-- 3079210's Lua and Manifest Created by Hubcap Manifest
+-- Hell Let Loose: Vietnam
+-- Created: August 13, 2026 at 12:01:16 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 1 (1 excluded)
+
+-- MAIN APPLICATION
+addappid(3079210) -- Hell Let Loose: Vietnam
+-- MAIN APP DEPOTS
+addappid(3079211, 1, "f7eb2462eb25f9a05bb91c0a20e77a6f943af632b06af7d4f2e19dd912176e6c") -- Depot 3079211
+setManifestid(3079211, "255077099873826850", 23310978959)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4447980) -- Field Supply NVA Boat Crew Uniform
+-- EXCLUDED DLCS:
+-- UNRELEASED DLCS (COMMENTED OUT)
+-- addappid(4447970) -- Field Supply: US Army Boat Crew Uniform (unreleased)
